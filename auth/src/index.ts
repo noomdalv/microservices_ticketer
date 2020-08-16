92ca1160a73c89102a5595195f29975196ffa49a
noomdalv <vladlunasan@gmail.com>
2020-08-15 20:01:00 -0500

    created custom error handler and subtypes for validation and database

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -5,6 +5,7 @@ import { currentUserRouter } from "./routes/current_user";
 import { signInRouter } from "./routes/signin";
 import { signOutRouter } from "./routes/signout";
 import { signUpRouter } from "./routes/signup";
+import { errorHandler } from "./middlewares/error_handler";
 
 const app = express();
 
@@ -14,6 +15,8 @@ app.use(currentUserRouter);
 app.use(signInRouter);
 app.use(signOutRouter);
 app.use(signUpRouter);
+
+app.use(errorHandler);
 
 app.listen(3000, () => {
 	console.log("Listening on port 3000");
--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,5 +1,7 @@
 import express, { Request, Response } from "express";
 import { body, validationResult } from "express-validator";
+import { RequestValidationError } from "../errors/request_validation_error";
+import { DatabaseConnectionError } from "../errors/database_connection_error";
 
 const router = express.Router();
 
@@ -16,12 +18,13 @@ router.post(
 		const errors = validationResult(req);
 
 		if (!errors.isEmpty()) {
-			return res.status(400).send(errors.array());
+			throw new RequestValidationError(errors.array());
 		}
 
 		const { email, password } = req.body;
 
 		console.log("Creating user...");
+		throw new DatabaseConnectionError();
 
 		res.send({});
 	}
