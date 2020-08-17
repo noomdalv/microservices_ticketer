2464e4c5fb11f5c173f8059d0b99513f3d7fa1a5
noomdalv <vladlunasan@gmail.com>
2020-08-17 18:54:30 -0500

    added user.save validation for signup route

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,7 +1,7 @@
 import express, { Request, Response } from "express";
 import { body, validationResult } from "express-validator";
 import { RequestValidationError } from "../errors/request_validation_error";
-import { DatabaseConnectionError } from "../errors/database_connection_error";
+import { User } from "../models/user";
 
 const router = express.Router();
 
@@ -23,10 +23,17 @@ router.post(
 
 		const { email, password } = req.body;
 
-		console.log("Creating user ...");
-		throw new DatabaseConnectionError();
+		const existingUser = await User.findOne({ email });
 
-		res.send({});
+		if (existingUser) {
+			console.log("Email already in use.");
+			return res.send({});
+		}
+
+		const user = User.build({ email, password });
+		await user.save();
+
+		res.status(201).send(user);
 	}
 );
 
