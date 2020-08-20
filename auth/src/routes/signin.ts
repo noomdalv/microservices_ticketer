66bfc6c038f0561e902144485953414f902aaf69
noomdalv <vladlunasan@gmail.com>
2020-08-19 19:36:10 -0500

    added validation/authentication for sign in route

--- auth/src/routes/signin.ts
+++ auth/src/routes/signin.ts
@@ -1,7 +1,11 @@
 import express, { Request, Response } from "express";
 import { body } from "express-validator";
+import jwt from "jsonwebtoken";
 
+import { User } from "../models/user";
+import { PasswordManager } from "../helpers/password_manager";
 import { validateRequest } from "../middlewares/validate_request";
+import { BadRequestError } from "../errors/bad_request_error";
 
 const router = express.Router();
 
@@ -15,7 +19,38 @@ router.post(
 			.withMessage("You must supply a password."),
 	],
 	validateRequest,
-	(req: Request, res: Response) => {}
+	async (req: Request, res: Response) => {
+		const { email, password } = req.body;
+
+		const existingUser = await User.findOne({ email });
+		if (!existingUser) {
+			throw new BadRequestError("Invalid credentials.");
+		}
+
+		const passwordMatch = await PasswordManager.compare(
+			existingUser.password,
+			password
+		);
+		if (!passwordMatch) {
+			throw new BadRequestError("Invalid credentials.");
+		}
+
+		// Generate JWT
+		const userJwt = jwt.sign(
+			{
+				id: existingUser.id,
+				email: existingUser.email,
+			},
+			process.env.JWT_KEY!
+		);
+
+		// store JWT on session
+		req.session = {
+			jwt: userJwt,
+		};
+
+		res.status(201).send(existingUser);
+	}
 );
 
 export { router as signInRouter };
