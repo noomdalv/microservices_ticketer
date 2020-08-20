ae915cffc11994dfd62a18a61cbf8c9f9d6c5a06
noomdalv <vladlunasan@gmail.com>
2020-08-20 17:22:55 -0500

    added jest, supertest and mongodb-memory-server  dev dependencies

---
+++ auth/src/app.ts
@@ -0,0 +1,36 @@
+import express from "express";
+import "express-async-errors";
+import { json } from "body-parser";
+import cookieSession from "cookie-session";
+
+import { currentUserRouter } from "./routes/current_user";
+import { signInRouter } from "./routes/signin";
+import { signOutRouter } from "./routes/signout";
+import { signUpRouter } from "./routes/signup";
+import { errorHandler } from "./middlewares/error_handler";
+import { NotFoundError } from "./errors/not_found_error";
+
+const app = express();
+
+app.set("trust proxy", true); // Traffic to the app is being handled through ingress nginx
+
+app.use(json());
+app.use(
+	cookieSession({
+		signed: false,
+		secure: true,
+	})
+);
+
+app.use(currentUserRouter);
+app.use(signInRouter);
+app.use(signOutRouter);
+app.use(signUpRouter);
+
+app.all("*", async (req, res) => {
+	throw new NotFoundError();
+});
+
+app.use(errorHandler);
+
+export { app };
