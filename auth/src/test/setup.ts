6ec1a38e177df595e9e7ddfa16847e417014ea13
noomdalv <vladlunasan@gmail.com>
2020-08-21 00:39:55 -0500

    created signup.test.ts with test cases for user validation

--- auth/src/test/setup.ts
+++ auth/src/test/setup.ts
@@ -1,16 +1,24 @@
 import { MongoMemoryServer } from "mongodb-memory-server";
-import mongoose, { connection, mongo } from "mongoose";
+import mongoose from "mongoose";
 import { app } from "../app";
 
 let mongo: any;
 beforeAll(async () => {
+	process.env.JWT_KEY = "asdfasdf";
+
 	mongo = new MongoMemoryServer();
 	const mongoUri = await mongo.getUri();
 
-	await mongoose.connect(mongoUri, {
-		useNewUrlParser: true,
-		useUnifiedTopology: true,
-	});
+	await mongoose.connect(
+		mongoUri,
+		{
+			useNewUrlParser: true,
+			useUnifiedTopology: true,
+		},
+		(err) => {
+			if (err) console.error(err);
+		}
+	);
 });
 
 beforeEach(async () => {
