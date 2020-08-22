5d92f780ee19dfe2b7b2ce42df07467c6e4d2d6d
noomdalv <vladlunasan@gmail.com>
2020-08-22 18:52:08 -0500

    added server side rendering for user object in landing page

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,5 +1,28 @@
-const index = () => {
-	return <h1>Index Page!!!</h1>;
+import axios from "axios";
+
+const LandingPage = ({ currentUser }) => {
+	console.log("currentuser = ", currentUser);
+	return <h1>Landing Page</h1>;
 };
 
-export default index;
+LandingPage.getInitialProps = async ({ req }) => {
+	if (typeof window === "undefined") {
+		// no browser window object defined(we are at the server side)
+		// requests should be made to "http://SERVICENAME.NAMESclsPACE.svc.cluster.local"
+		const { data } = await axios.get(
+			"http://ingress-nginx-controller.ingress-nginx.svc.cluster.local/api/users/currentuser",
+			{
+				headers: req.headers,
+			}
+		);
+		return data;
+	} else {
+		// request is made from the browser(not server)
+		const { data } = await axios.get("/api/users/currentuser");
+		return data;
+	}
+
+	return {};
+};
+
+export default LandingPage;
