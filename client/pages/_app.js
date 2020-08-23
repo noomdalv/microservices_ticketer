dffcbbcfa0961b9d9bd78e683edbdb4fd5f80778
noomdalv <vladlunasan@gmail.com>
2020-08-23 03:03:26 -0500

    created signout page to handle user sign out

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,17 +1,17 @@
 import "bootstrap/dist/css/bootstrap.css";
 import buildClient from "../api/build_client";
+import Header from "../components/header";
 
 const AppComponent = ({ Component, pageProps, currentUser }) => {
 	return (
 		<div>
-			<h1>Header {currentUser.email}</h1>
+			<Header currentUser={currentUser} />
 			<Component {...pageProps} />
 		</div>
 	);
 };
 
 AppComponent.getInitialProps = async (appContext) => {
-	console.log("appContext = ", appContext);
 	const client = buildClient(appContext.ctx);
 	const { data } = await client.get("/api/users/currentuser");
 
