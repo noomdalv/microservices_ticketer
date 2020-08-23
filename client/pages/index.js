07bb00aebce2e314bcfa53ce500bf2d75077b195
noomdalv <vladlunasan@gmail.com>
2020-08-22 19:04:31 -0500

    refactored getInitialProps into build_client.js with axios config file

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,28 +1,15 @@
-import axios from "axios";
+import buildClient from "../api/build_client";
 
 const LandingPage = ({ currentUser }) => {
 	console.log("currentuser = ", currentUser);
 	return <h1>Landing Page</h1>;
 };
 
-LandingPage.getInitialProps = async ({ req }) => {
-	if (typeof window === "undefined") {
-		// no browser window object defined(we are at the server side)
-		// requests should be made to "http://SERVICENAME.NAMESclsPACE.svc.cluster.local"
-		const { data } = await axios.get(
-			"http://ingress-nginx-controller.ingress-nginx.svc.cluster.local/api/users/currentuser",
-			{
-				headers: req.headers,
-			}
-		);
-		return data;
-	} else {
-		// request is made from the browser(not server)
-		const { data } = await axios.get("/api/users/currentuser");
-		return data;
-	}
+LandingPage.getInitialProps = async (context) => {
+	const client = buildClient(context);
+	const { data } = await client.get("/api/users/currentuser");
 
-	return {};
+	return data;
 };
 
 export default LandingPage;
